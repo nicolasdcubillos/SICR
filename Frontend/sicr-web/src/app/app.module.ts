3d2f8554b5fd61567863631a32c8e1213ee4a0c5
esrojas27 <esrojas27@gmail.com>
2023-05-23 14:24:31 -0500

    new branch

--- Frontend/sicr-web/src/app/app.module.ts
+++ Frontend/sicr-web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
