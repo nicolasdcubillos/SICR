037abce28652e80772fbbd7dd42dc155d247b7d5
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2023-05-28 06:17:12 -0500

    Ajuste kubectl IBM Cloud | ms-admon.

--- Frontend/sicr-web-publico/src/environments/environment.development.ts
+++ Frontend/sicr-web-publico/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    MS_RESERVAS_URL: 'http://localhost:8084',
-    MS_ADMON_URL: 'http://localhost:8080'
+    MS_ADMON_URL: 'http://localhost:8080',
+    MS_RESERVAS_URL: 'http://localhost:8084'
 };
