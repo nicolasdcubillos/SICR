12021f44bc89e4ac672c887c6596d545b071eb75
Angello <anmajari2345@gmail.com>
2023-05-29 19:07:52 -0500

    Kubernetes

--- Frontend/sicr-web-publico/src/environments/environment.ts
+++ Frontend/sicr-web-publico/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     MS_ADMON_URL: 'http://169.51.194.59:31120',
     MS_RESERVAS_URL: 'http://localhost:8084',
+    MS_PEDIDOS_URL : 'http://169.51.206.232:32661'
 };
